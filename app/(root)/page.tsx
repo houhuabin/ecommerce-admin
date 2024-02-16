b7cd27f7587b423417bac5799417996e309f7656
houhuabin <houhuabin@163.com>
2024-02-17 08:44:48 +1300

    add zustand modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,38 @@
-import Image from "next/image";
+"use client";
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import { Modal } from "@/components/ui/modal";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useEffect, useState } from "react";
+import { useStoreModal } from "@/hooks/use-store-modal";
 const SetupPage = () => {
+  const [domLoaded, setDomLoaded] = useState(false);
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    setDomLoaded(true);
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
   return (
-    <div className="p-4">
-      <Button>Click me</Button>
-      <div className="h-screen">
-        <UserButton afterSignOutUrl="/" />
-      </div>
-    </div>
+    <>
+      {domLoaded && (
+        <div className="p-4">
+          <Button>Click me</Button>
+        </div>
+      )}
+    </>
   );
 };
 export default SetupPage;
